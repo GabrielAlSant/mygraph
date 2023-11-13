57bc6ed77fba41ee91e7d8116fb600fefe1b4378
Gabriel Alves dos Santos <gabrielalvesdossantos16@gmail.com>
2023-11-12 21:34:48 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import ReactEcharts from 'echarts-for-react'; // importe o componente do pacote correto
 
 function App() {
+  const options = {
+    title: { 
+      text: 'Grafico'
+    },
+    legend: {
+      data: ['Zed', 'Kayn']
+    },
+    radar: {
+      
+      indicator: [
+        { name: 'Força', max: 6500 },
+        { name: 'Velocidade', max: 16000 },
+        { name: 'QI', max: 60000 },
+        { name: 'Poder Mágico', max: 38000 },
+        { name: 'Persuasão', max: 52000 },
+        { name: 'Força Física', max: 25000 }
+      ]
+    },
+    series: [
+      {
+        name: 'Zed vs Kayn',
+        type: 'radar',
+        data: [
+          {
+            value: [4200, 3000, 20000, 35000, 50000, 18000],
+            name: 'Zed'
+          },
+          {
+            value: [5000, 14000, 28000, 26000, 42000, 21000],
+            name: 'Kayn'
+          }
+        ]
+      }
+    ]
+  };
+
+  const options1 = {
+    title: { 
+      text: 'Grafico'
+    },
+    legend: {
+      data: ['Zead', 'Kayan']
+    },
+    radar: {
+      
+      indicator: [
+        { name: 'Força', max: 6500 },
+        { name: 'Velocidade', max: 16000 },
+        { name: 'QI', max: 60000 },
+        { name: 'Poder Mágico', max: 38000 },
+        { name: 'Persuasão', max: 52000 },
+        { name: 'Força Física', max: 25000 }
+      ]
+    },
+    series: [
+      {
+        name: 'Zed vs Kayn',
+        type: 'radar',
+        data: [
+          {
+            value: [4200, 3000, 20000, 35000, 50000, 18000],
+            name: 'Zed'
+          },
+          {
+            value: [5000, 14000, 28000, 26000, 42000, 21000],
+            name: 'Kayn'
+          }
+        ]
+      }
+    ]
+  };
+
+  const options2 = {
+    title: { 
+      text: 'Grafico'
+    },
+    legend: {
+      data: ['Zed', 'Kayn']
+    },
+    radar: {
+      
+      indicator: [
+        { name: 'Força', max: 6500 },
+        { name: 'Velocidade', max: 16000 },
+        { name: 'QI', max: 60000 },
+        { name: 'Poder Mágico', max: 38000 },
+        { name: 'Persuasão', max: 52000 },
+        { name: 'Força Física', max: 25000 }
+      ]
+    },
+    series: [
+      {
+        name: 'Zed vs Kayn',
+        type: 'radar',
+        data: [
+          {
+            value: [4200, 3000, 20000, 35000, 50000, 18000],
+            name: 'Zed'
+          },
+          {
+            value: [5000, 14000, 28000, 26000, 42000, 21000],
+            name: 'Kayn'
+          }
+        ]
+      }
+    ]
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+      <div className="App">
+        <ReactEcharts option={options} style={{height: '400px', width: '600px'}} />
+
+        
+      </div>
+    );
 }
 
 export default App;
