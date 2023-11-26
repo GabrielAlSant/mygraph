539789ac81b3a5fa19844267a17a7fad569dbe91
Gabriel Alves dos Santos <gabrielalvesdossantos16@gmail.com>
2023-11-26 18:25:55 -0300

    att front

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css'
 import ReactEcharts from 'echarts-for-react'; // importe o componente do pacote correto
 import ney from './ney.png'
@@ -9,6 +9,10 @@ import vini from './vini.png'
 import mar from './marcelo.png'
 
 function App() {
+
+  const [selectedPlayer, setSelectedPlayer] = useState(null);
+  const [selectedValue, setSelectedValue] = useState([]);
+
   const options = {
     title: { 
       text: ''
@@ -19,12 +23,12 @@ function App() {
     radar: {
       
       indicator: [
-        { name: 'Força', max: 10, nameTextStyle: { fontSize: 25 } },
-        { name: 'Velocidade', max: 10, nameTextStyle: { fontSize: 25 } },
-        { name: 'Técnica', max: 10 , nameTextStyle: { fontSize: 25 }},
-        { name: 'Poder do Chute', max: 10 , nameTextStyle: { fontSize: 25 }},
-        { name: 'Disparada', max: 10, nameTextStyle: { fontSize: 25 } },
-        { name: 'Salto', max: 10 , nameTextStyle: { fontSize: 25 }}
+        { name: 'Força', max: 10, nameTextStyle: { fontSize: 20 } },
+        { name: 'Velocidade', max: 10, nameTextStyle: { fontSize: 20 } },
+        { name: 'Técnica', max: 10 , nameTextStyle: { fontSize: 20 }},
+        { name: 'Precisão', max: 10 , nameTextStyle: { fontSize: 20 }},
+        { name: 'Disparada', max: 10, nameTextStyle: { fontSize: 20 } },
+        { name: 'Salto', max: 10 , nameTextStyle: { fontSize: 20 }}
       ]
     },
     series: [
@@ -32,68 +36,81 @@ function App() {
         name: 'Jogadores',
         type: 'radar',
         data: [
-          {
-            value: [4, 3, 2, 3, 5, 1],
-            name: 'Neymar'
-          },
-          {
-            value: [5, 1, 2, 2, 4, ],
-            name: 'Messi'
-          },
-          {
-            value: [5, 2,8,7,4,2,3],
-            name: 'CR 7'
-          },
-          {
-            value: [5, 1, 8, 3, 4, 2],
-            name: 'Mbape'
-          }
+          
         ]
       }
     ]
   };
 
-  
   return (
-
-      <div className="app">
-         <h1>Radar de Jogadores</h1>
-       <div className='App'>
-       <div className='esquerdo'>
-          <div className='card'>     
-            <img src={ney}></img>
+    <div className="app">
+     <h1 className='titulo'>Radar de Jogadores</h1>
+      <div className='App'>
+        <div className='esquerdo'>
+        
+          <div className='card' onClick={() => [setSelectedPlayer('Neymar'), setSelectedValue([4, 3, 2, 3, 5, 1] )]}>
+          <img src={ney}></img>
             <div>N E Y M A R</div>
-         
           </div>
-          <div className='card'>
-            <img src={messi}></img>
+          <div className='card' onClick={() => [setSelectedPlayer('Messi'), setSelectedValue([5, 2,8,7,4,2])]}>
+          <img src={messi}></img>
              <div>M E S S I</div>
           </div>
-          <div className='card'>
-            <img src={mb}></img>
+          <div className='card' onClick={() => [setSelectedPlayer('Mbappe'), setSelectedValue([5,2,1,5,6,8])]}>
+          <img src={mb}></img>
             <div>M B A P P E</div>
           </div>
-          <div className='card'>
-            <img src={cr7}></img>
+          <div className='card' onClick={() => [setSelectedPlayer('Cristiano Ronaldo'), setSelectedValue([5,2,1,5,6,8])]}>
+          <img src={cr7}></img>
             <div>R O N A L D O</div>
           </div>
-          <div className='card'>
-            <img src={vini}></img>
-            <div> V I N I</div>
+          <div className='card' onClick={() => [setSelectedPlayer('Vini'), setSelectedValue([8,3,4,2,5,9])]}>
+          <img src={vini}></img>
+            <div>V I N I</div>
           </div>
-          <div className='card'>
-            <img src={mar}></img>
-            <div> M A R C E L O</div>
+          <div className='card' onClick={() => [setSelectedPlayer('Marcelo'), setSelectedValue([6,4,2,9,6,2])]}>
+          <img src={mar}></img>
+            <div>M A R C E L O</div>
           </div>
         </div>
-     
-        <div className='direito'>
-        <ReactEcharts option={options} className='grafico' style={{  height: '600px',background:'none', border:'none', fontSize:'10px'}} />
 
+        <div className='direito'>
+        {(
+            <ReactEcharts
+              option={{
+                ...options,
+                series: [
+                  {
+                    ...options.series[0],
+                    data: [
+                      {
+                        value: selectedValue,
+                        name: selectedPlayer
+                      }
+                    ]
+                  }
+                ]
+              }}
+              className='grafico'
+              style={{ height: '400px', background: 'none', border: 'none' }}
+            />
+          )}
+           <div className='status'>
+            <h3>Nome do Jogador: {selectedPlayer || 'Não há nenhum Selecionado'}</h3>
+            <div className='valor'>
+            <div className='att'>Força: <div>{selectedValue[0] || 0}</div></div>
+            <div className='att'>Velocidade: <div>{selectedValue[1] || 0}</div></div>
+            <div className='att'>Técnica: <div>{selectedValue[2] || 0}</div></div>
+            <div className='att'>Precisão: <div>{selectedValue[3] || 0}</div></div>
+            <div className='att'>Disparada: <div>{selectedValue[4] || 0}</div></div>
+            <div className='att'>Salto: <div>{selectedValue[5] || 0}</div></div>
+            </div>
+         </div>
+         
         </div>
-       </div>
       </div>
-    );
+    </div>
+  );
 }
 
 export default App;
